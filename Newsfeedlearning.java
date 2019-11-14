77b67d4e672587bba2b9fd52faa64866b62dc882
DevelopmentNow <allynyork@gmail.com>
2019-11-14 15:07:57 -0800

    Newsfeedlearning

--- Newsfeedlearning.java
+++ Newsfeedlearning.java
@@ -1,15 +1,24 @@
 public class Newsfeedlearning {
   
-  
+  // constructor
   public Newsfeedlearning(){
     
   }
   
+  // First, make a method called getTopics(), which:
+  // is public
+  // returns a String array
+  // does not take any parameters
+  // For now, leave the method empty
   public String[] getTopics(){
+    // Inside the getTopics() method, create a String array called topics and set it equal to an array containing these elements, in order: 
+    // Opinion Tech Science Health
     String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    // Then, return topics from the method
     return topics;
   }
 
+  // main method
   public static void main(String[] args){
     Newsfeedlearning sampleFeed = new Newsfeedlearning();
     /*
